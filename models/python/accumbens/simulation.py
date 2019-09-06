45d50fec29d6a14da774b989c050e049d13e52e0
aljiro <aljiro@gmail.com>
2019-09-06 16:49:53 +0100

    Example of accumbens driven action selection

---
+++ models/python/accumbens/simulation.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import matplotlib.plot as plt 
+
+import RDDR;
+
+class Model:
+	def __init__(self):
+		
+
+class Solver:
+	def __init__(self):
+
+if __name__ == "__main__":
+	model = Model()
+	solver = Solver
+	s.integrate(model, 100);
