5ab96a6e47c7e453ed4d6ed2eacaafdb4b1c063a
David Buxton <dbuxton+github@gmail.com>
2020-09-30 12:31:57 +0100

    Minor improvement to closeness detection

--- models/python/hypothalamus/dynamical/audio_perception.py
+++ models/python/hypothalamus/dynamical/audio_perception.py
@@ -9,9 +9,11 @@ MICS = 4
 class AudioPerception:
 	def __init__(self):
 		# Audio frequency parameters
+		# TODO: Allow frequencies to be defined when creating class object
 		self.frequencies = [440, 600]       # Frequencies to detect (multiples of 40 recommended)
 		self.buffer_length = 20             # At 40Hz a buffer of 20 == 0.5s
-		self.f_thresh = 0.2                  # Minimum power at which to register frequency detection
+		self.f_thresh = 0.2                 # Minimum power at which to register frequency detection
+		self.c_power = 10                    # Frequency power at which MiRo is 'close' to audio source
 
 		# FFT array positions of defined frequencies
 		self.f_pos = [int(frq / BLOCK_RATE) for frq in self.frequencies]
@@ -21,12 +23,15 @@ class AudioPerception:
 	@staticmethod
 	def shape_data(data):
 		# Reshape data (from node_detect_audio_engine.py)
+		# TODO: Catch error when incorrect size array is passed
+		# TODO: Move this functionality into the mics callback
 		data = np.asarray(data, 'float32') * (1.0 / 32768.0)
 		data = data.reshape((MICS, SAMP_PER_BLOCK))
 
 		return data
 
 	def do_fft(self, data):
+		# TODO: Rename this 'step' and perform every time mics_callback is activated, store FFT data in self structure
 		# Perform Fast Fourier Transform on audio sample from each microphone, dropping mirrored and imaginary portions
 		fft_data = [np.abs(np.fft.fft(mic))[: SAMP_PER_BLOCK / 2] for mic in data]
 
@@ -40,9 +45,6 @@ class AudioPerception:
 		return freq_power
 
 	def locate_frequencies(self, data):
-		# # Reshape data (from node_detect_audio_engine.py)
-		# data = np.asarray(data, 'float32') * (1.0 / 32768.0)
-		# data = data.reshape((MICS, SAMP_PER_BLOCK))
 		data = self.shape_data(data)
 
 		# # Perform Fast Fourier Transform on audio sample from each microphone, dropping mirrored and imaginary portions
@@ -82,16 +84,19 @@ class AudioPerception:
 
 	def isClose(self, data):
 		data = self.shape_data(data)
-
 		freq_power = self.do_fft(data)
 
-		# PICKED RANDOM NUMBER FOR POWER LEVEL
-		# TODO: Update to play nice with more frequencies
-		print('freq power: ' + str(freq_power[0][3]))
-		if freq_power[0][3] > 5:
-			return 1
-		else:
-			return 0
+		close_val = np.zeros(len(freq_power))
+
+		for frq, _ in enumerate(freq_power):
+			# Use centre mic to gauge closeness
+			close_val[frq] = freq_power[frq][2]
+			if close_val[frq] >= self.c_power:
+				close_val[frq] = 1
+			else:
+				close_val[frq] = 0
+
+		return close_val
 
 
 
