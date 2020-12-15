f93391a317b81fc5060acd16875114cba09890f4
aljiro <aljiro@gmail.com>
2020-12-15 03:05:51 +0000

    Experimenting with solutions to the model

--- models/python/hypothalamus/dynamical/potential_shapes.py
+++ models/python/hypothalamus/dynamical/potential_shapes.py
@@ -1,11 +1,15 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-a = 1.
-b = 0.95
+a = 0.
+b = 0.
 rhos = np.linspace(-1.1,1.1,100)
 U1 = lambda rho, a, b: (rho - 1)**2*(rho + 1)**2 + a*(rho + 1)**2 + b*(rho - 1)**2
 
 plt.figure()
-plt.plot( rhos, U1(rhos,a ,b))
+s = U1(rhos,a ,b)
+M = s[50]
+m = np.amax(-s)
+print( M+m )
+plt.plot( rhos, s)
 plt.show()
